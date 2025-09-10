8f5bf654543341ecee12c0b3b98fd3bed33d8863
axonkid <cbeltre0626@gmail.com>
2025-09-09 23:47:42 -0400

    completed rock-paper-scissors project

---
+++ rock-paper-scissors.py
@@ -0,0 +1,35 @@
+import random
+#Rock Paper Scissors Game
+user_choose = input("What do you choose? Type 'rock', 'paper' or 'scissors': ").lower() #.lower() makes input case insensitive
+#0 = rock, 1 = paper, 2 = scissors
+computer_choose = random.randint(0,2)
+if computer_choose == 0:
+    print('Computer chose: "rock"')
+    if user_choose == "rock":
+        print("It's a draw")
+    elif user_choose == "paper":
+        print("You win!")
+    elif user_choose == "scissors":
+        print("You lose!")
+    else:
+        print("Invalid input, you lose!")
+elif computer_choose == 1:
+    print('Computer chose: "paper"')
+    if user_choose == "rock":
+        print("You lose!")
+    elif user_choose == "paper":
+        print("It's a draw")
+    elif user_choose == "scissors":
+        print("You win!")
+    else:
+        print("Invalid input, you lose!")
+elif computer_choose == 2:
+    print('Computer chose: "scissors"')
+    if user_choose == "rock":
+        print("You win!")
+    elif user_choose == "paper":
+        print("You lose!")
+    elif user_choose == "scissors":
+        print("It's a draw")
+    else:
+        print("Invalid input, you lose!")
