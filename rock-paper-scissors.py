2d54f8520cb965e0d156615c073f034e0932c43f
axonkid <cbeltre0626@gmail.com>
2025-09-15 12:15:11 -0400

    "2025-09-15 Initial Commit"

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,18 +1,45 @@
 import random
 #Rock Paper Scissors Game
-user_choose = input("What do you choose? Type 'rock', 'paper' or 'scissors': ").lower() #.lower() makes input case insensitive
-#0 = rock, 1 = paper, 2 = scissors
+user_choose = input("What do you choose? Type 0 for 'rock', 1 for 'paper' 2 for 'scissors': ") #0 = rock, 1 = paper, 2 = scissors
 computer_choose = random.randint(0,2)
+#Rock
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+#Paper
+paper = '''
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+'''
+#Scissors
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
 if computer_choose == 0:
-    print('Computer chose: "rock"')
-    if user_choose == "rock":
+    print("Computer chose: {rock}")
+    if user_choose == 0:
         print("It's a draw")
-    elif user_choose == "paper":
+    elif user_choose == 1:
         print("You win!")
-    elif user_choose == "scissors":
+    elif user_choose == 2:
         print("You lose!")
     else:
         print("Invalid input, you lose!")
+'''
 elif computer_choose == 1:
     print('Computer chose: "paper"')
     if user_choose == "rock":
@@ -33,3 +60,4 @@ elif computer_choose == 2:
         print("It's a draw")
     else:
         print("Invalid input, you lose!")
+        '''
